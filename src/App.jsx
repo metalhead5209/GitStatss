737778cb5d3746ea03c85d48c55de9f71b573d10
Aaron Rodi <metalhead5209@gmail.com>
2022-09-08 04:02:30 -0500

    initial boiler plate, implented router with home, login, and error page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import ErrorPage from './pages/ErrorPage';
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
