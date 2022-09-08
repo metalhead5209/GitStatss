5557e3e65d2ea451424c3953412961528cf32d05
Aaron Rodi <metalhead5209@gmail.com>
2022-09-08 09:23:39 -0500

    ready to start setting up home page with mock data

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import MockUser from './mockData.jsx/mockUser';
+import MockFollowrs from './mockData.jsx/mockFollowers';
+import MockRepos from './mockData.jsx/mockRepos';
 
 const rootUrl = 'https://api.github';
 
