8f9e0edbea4cdc7fdb78504c29adfbdf139099e9
Aaron Rodi <metalhead5209@gmail.com>
2022-10-01 14:39:23 -0500

    built wrapper for repo stats section

---
+++ src/components/RepoStats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RepoStatsWrapper } from '../Wrappers';
+import { GitContext } from '../Context/context';
+
+const RepoStats = () => {
+  return (
+    <RepoStatsWrapper>
+        
+    </RepoStatsWrapper>
+  )
+}
+
+export default RepoStats
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import NavBar from "../components/NavBar";
+import RepoStats from "../components/RepoStats";
 import User from "../components/User";
 import UserData from "../components/UserData";
 import { ErrorWrapper } from "../Wrappers";
@@ -11,6 +12,7 @@ const Home = () => {
         <NavBar />
         <UserData />
         <User />
+        <RepoStats />
     </>
   );
 };
