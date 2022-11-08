710314268beb85c402dbda5c837b95cc3e3d8f92
Aaron Rodi <metalhead5209@gmail.com>
2022-11-08 07:53:22 -0600

    added login, logout functionality with auth0, still need to set up private route for auth users

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,47 @@ import React from "react";
 import styled from "styled-components";
 import { GitContext } from "../Context/context";
 import { NavWrapper } from "../Wrappers";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
-  const data = React.useContext(GitContext);
-  // console.log(data);
-  return <NavWrapper></NavWrapper>;
+  const { 
+    isLoading, 
+    isAuthenticated, 
+    error, 
+    user, 
+    loginWithRedirect, 
+    logout 
+  } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading....</div>
+  };
+  if (error) {
+    return <div>Oops...{error.message}</div>
+  };
+
+  const USER = isAuthenticated && user;
+
+  console.log({user, isAuthenticated, isLoading});
+
+  return (
+    <NavWrapper>
+      {USER && user.picture && <img src={user.picture} alt='userpic' /> }
+      {USER && user.name && <h3>Welcome, <b>{user.name}</b></h3>}
+      {USER  ?
+        <button
+        onClick={() => {
+          logout({ returnTo: window.location.origin });
+        }}
+      >
+        Logout
+      </button>:
+      <button onClick={loginWithRedirect}>Login</button>
+    }
+      
+      
+    </NavWrapper>
+  );
 };
 
 export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 /* xwTQ0SJT7rlvpNBl0zCJl7a6vhB4GSyR */
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider>
+  <Auth0Provider
     domain='dev-3q476l736bqtlne5.us.auth0.com'
     clientId='xwTQ0SJT7rlvpNBl0zCJl7a6vhB4GSyR'
     redirectUri={window.location.origin}
+    >
     <GitProvider>
       <App />
     </GitProvider>
